d1e6b4bfcc4ea87942be23783c78d5fdc0d1d85c
tsune <tsune@aluco.jp>
2025-06-23 13:01:28 +0900

    [Fix] 選手sy

--- src/components/player/PlayerImage.tsx
+++ src/components/player/PlayerImage.tsx
@@ -5,12 +5,24 @@ interface Props {
   name?: string;
 }
 
-export const PlayerImage = ({ playerId, name }: Props) => (
-  <div className="w-full flex justify-center mb-8">
-    <img
-      src={getPlayerImageUrlWithFallback(playerId)}
-      alt={`${name}のプロフィール画像`}
-      className="w-200 object-cover"
-    />
-  </div>
-)
+export const PlayerImage = ({ playerId, name }: Props) => {
+  console.log(playerId);
+
+  return (
+    <div className="w-full flex justify-center mb-8">
+      {playerId ?
+        <img
+          src={getPlayerImageUrlWithFallback(playerId)}
+          alt={`${name}のプロフィール画像`}
+          className="w-200 object-cover"
+        />
+        :
+        <img
+          src="/src/assets/img/dummy.jpg"
+          alt="ダミー画像"
+          className="w-200 object-cover"
+        />
+      }
+      </div>
+  )
+}
