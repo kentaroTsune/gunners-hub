39df86543e14a237cfeb4d15a8ed8806f76097f9
tsune <tsune@aluco.jp>
2026-06-01 18:55:24 +0900

    [Remove] 不要なファイル削除、ファイル統一

---
+++ src/constants/login.ts
@@ -0,0 +1,9 @@
+export const LOGIN_REDIRECT_PATH = '/';
+
+export const LOGIN_MESSAGES = {
+  TITLE: 'ログイン',
+  DESCRIPTION: 'Googleアカウントでログインしてください',
+  BUTTON_LOGIN: 'Googleでログイン',
+  BUTTON_LOADING: 'ログイン中...',
+  ARIA_LABEL: 'Googleアカウントでログイン',
+} as const;
